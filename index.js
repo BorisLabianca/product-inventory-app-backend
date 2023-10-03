d3a7fa13441d6932936cc885838cbc08ace2c84a
Boris Labianca <boris.labianca@hotmail.fr>
2023-10-03 18:56:05 +0800

    set up cors for credentials

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: false }));
-app.use(cors());
+app.use(cors({ origin: ["http://localhost:5173"], credentials: true }));
 app.use(bodyParser.json());
 
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
